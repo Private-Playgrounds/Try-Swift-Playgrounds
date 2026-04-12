4ff0cc306107e0726066faf4d71786f9f7ce4573
Chiharu Nameki <nameki_chiharu@cyberagent.co.jp>
2026-04-12 15:26:28 +0900

    Add _quietlySetVariantIndex sample

--- Playgrounds/ContentView.swift
+++ Playgrounds/ContentView.swift
@@ -34,6 +34,11 @@ public struct ContentView: View {
                     } label: {
                         Text("LiquidGlassSample")
                     }
+                    NavigationLink {
+                        QuietlySetVariantIndexView()
+                    } label: {
+                        Text("_quietlySetVariantIndex")
+                    }
                 }
                 .navigationTitle("Playgrounds")
             }
